138fdb7b27302fd8ea17b91f327054b523f8db47
hXcJay4130 <blacklisted4life@gmail.com>
2023-12-21 17:50:52 +0100

    first deploy

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
             <nav className="navbar__nav">
                 {/* <NavLink className="navbar__link" to="/">Accueil</NavLink>
                 <NavLink className="navbar__link" to="/about">A propos</NavLink> */}
-                <NavLink className="navbar__link" to="/">A propos de moi</NavLink>
+                <NavLink className="navbar__link" to="/">A propos</NavLink>
                 <NavLink className="navbar__link" to="/portfolio">Portfolio</NavLink>
                 <NavLink className="navbar__link" to="/schooling">Formation</NavLink>
                 <NavLink className="navbar__link" to="/skills">Compétences</NavLink>
